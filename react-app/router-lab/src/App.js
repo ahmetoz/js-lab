41338893770b29a3efce382e8e6baabdecfda62c
ahmetoz <bilmuhahmet@gmail.com>
2018-05-09 23:00:34 +0300

    bacic url params with react-router

--- react-app/router-lab/src/App.js
+++ react-app/router-lab/src/App.js
@@ -6,53 +6,12 @@ import {
   Link
 } from 'react-router-dom';
 
-const Home = () => (
+const Child = ({ match }) => console.log(match) || (
   <div>
-    <h2>
-      Home
-    </h2>
+    <h3>Id: {match.params.id}</h3>
   </div>
 )
 
-const Topic = ({ match }) => console.log(match) || (
-  <h2>{match.params.topicId}</h2>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>
-      Topics
-    </h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={`${match.url}`} component={() => (
-        <h2>Select a topic!</h2>
-      )} />
-  </div>
-)
-const About = () => (
-  <div>
-    <h2>
-      About
-    </h2>
-  </div>
-)
 
 class App extends Component {
   render() {
@@ -60,14 +19,12 @@ class App extends Component {
       <Router>
         <div className="App">
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/topics">Topics</Link></li>
+            <li><Link to="/netflix">Netflix</Link></li>
+            <li><Link to="/zillow-group">Zillow Group</Link></li>
+            <li><Link to="/yahoo">Yahoo</Link></li>
+            <li><Link to="modus-create">Modus Create</Link></li>
           </ul>
-          <hr />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/topics" component={Topics} />
+          <Route path="/:id" component={Child} />
         </div>
       </Router>
     );
